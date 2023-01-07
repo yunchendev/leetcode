fcbad4aae764e52c5a71e546f488095d6d3f3abe
yunchendev <98488691+yunchendev@users.noreply.github.com>
2023-01-07 03:09:25 -0500

    Time: 1 ms (99.56%), Space: 40.3 MB (84.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
         
+        // convert the input string into a character array
+        // create a stack 
+        // for every opening bracket, push a closing bracket onto the stack
+        // when current peek doesn't match the character it means the stack is no longer valid
+        // stack should not be empty before reaching the end of the character array
+        
         char[] arr = s.toCharArray();
         Stack<Character> stack = new Stack();
         
